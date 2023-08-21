9cef8798f8b361cd968e64558f45a8ea5a23c91f
Your Name <you@example.com>
2023-08-21 12:51:50 +0100

    update

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-# React + Vite
+# Rock, Paper, Scissors
+ 
+ This is a simple web example of the game which has only been created in the Front end. 
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Currently, two official plugins are available:
+## 📦Dependencies
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-# Rock-Paper-Scissors-Game
+1. Run `npm install`
+
+2. Run `npm run dev`
+
+
+Enjoy!
---
+++ index.js
@@ -0,0 +1,62 @@
+  const choices = ['✊', '🤚', '✌️'];
+  const player1 = document.getElementById('player-1');
+
+  /* 1a. */
+  const player2 = document.getElementById('player-2');
+
+  /* 1b. */
+  const resultArea = document.getElementById('result-area');
+
+  /* 1c. */
+  const playBtn = document.getElementById('play-btn');
+
+  /* This will help check your results */
+  console.log(player2, resultArea, playBtn);
+
+/* ----------------------------------------------- */
+
+/* --- FUNCTIONS --- */
+
+  const generateChoice = () => {
+    let r = Math.floor(Math.random() * 3);
+    return choices[r]
+  };
+
+  const insertHTML = (choice1, choice2, result) => {
+    player1.innerHTML = choice1;
+    player2.innerHTML = choice2;
+    resultArea.innerHTML = result;
+    resultArea.style.color = "black";
+  };
+
+
+  const decideWinner = (a, b) => {
+    if ((a === '✊' && b === '✊') || (a === '🤚' && b === '🤚') || (a === '✌️' && b === '✌️')) {
+
+      /* 3a. */ return "It's a tie!";
+
+    } else if ((a === '✊' && b === '✌️') || (a === '🤚' && b === '✊') || (a === '✌️' && b === '🤚')) {
+
+      /* 3b. */ return "Player one wins!";
+
+    } else {
+
+      /* 3c. */ return "Player 2 wins!";
+
+    }
+  };
+
+  const play = () => {
+    let choice1 = generateChoice();
+    let choice2 = generateChoice();
+    let result = decideWinner(choice1, choice2);
+
+    insertHTML(choice1, choice2, result);
+  };
+
+/* ----------------------------------------------- */
+
+/* --- EVENT LISTENERS --- */
+
+playBtn.addEventListener('click', play);
+/* ------------------------------- */
